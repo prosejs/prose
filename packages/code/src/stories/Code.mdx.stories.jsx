ac4030641a370fe499b61d561f865a67c6413af3
devdigital <devdigital.cloud@gmail.com>
2020-02-26 09:33:21 +0000

    chore: refactor code stories

---
+++ packages/code/src/stories/Code.mdx.stories.jsx
@@ -0,0 +1,74 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import MdxThemed from './MdxThemed'
+
+export default { title: 'code/mdx' }
+
+const defaultProps = {
+  code: `
+const foo = 'bar'
+const bar = 'foo'
+`,
+  language: null,
+}
+
+export const withNoLanguage = () => <MdxThemed {...defaultProps} />
+
+export const withJavascript = () => (
+  <MdxThemed {...defaultProps} meta="javascript" />
+)
+
+export const withJavascriptWhitespace = () => (
+  <MdxThemed {...defaultProps} meta="   javascript   " />
+)
+
+export const withJavascriptAndOptions = () => (
+  <MdxThemed {...defaultProps} meta="javascript numbers:{enabled start:5}" />
+)
+
+export const withNoneLanguageAndOptions = () => (
+  <MdxThemed {...defaultProps} meta="none numbers:{enabled start:5}" />
+)
+
+export const withInvalidNoLanguageAndOptions = () => (
+  <MdxThemed {...defaultProps} meta="numbers:{enabled start:5}" />
+)
+
+export const withEmphasiseLines = () => (
+  <MdxThemed {...defaultProps} meta="javascript em:[2]" />
+)
+
+export const withEmphasiseLinesAdd = () => (
+  <MdxThemed {...defaultProps} meta="javascript em:[2+]" />
+)
+
+export const withEmphasiseLinesSubtract = () => (
+  <MdxThemed {...defaultProps} meta="javascript em:[2-]" />
+)
+
+export const withDeemphasiseLines = () => (
+  <MdxThemed {...defaultProps} meta="javascript dem:[2]" />
+)
+
+export const withDeemphasiseLinesAdd = () => (
+  <MdxThemed {...defaultProps} meta="javascript dem:[2+]" />
+)
+
+export const withDeemphasiseLinesSubtract = () => (
+  <MdxThemed {...defaultProps} meta="javascript dem:[2-]" />
+)
+
+export const withDiff = () => {
+  const code = `+ const foo = 'bar'
+- const bar = 'foo'`
+
+  return <MdxThemed {...defaultProps} meta="diff" code={code} />
+}
+
+export const withLineNumbersDisabled = () => (
+  <MdxThemed {...defaultProps} meta="javascript lines:{enabled:false}" />
+)
+
+export const withTitle = () => (
+  <MdxThemed {...defaultProps} meta={`javascript title:'./foo/bar.js'`} />
+)
