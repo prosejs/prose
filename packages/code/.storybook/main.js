2aa897eb08d9cabd799342715df76ad248057e48
devdigital <devdigital.cloud@gmail.com>
2020-11-04 23:28:34 +0000

    chore: add storybook webpack config

--- packages/code/.storybook/main.js
+++ packages/code/.storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  addons: ['@storybook/addon-knobs/register'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
     config.node = {
       fs: 'empty',
